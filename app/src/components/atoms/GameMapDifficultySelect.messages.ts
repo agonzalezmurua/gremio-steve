0b400e71f42259f7a0551b1e143fcf116e683127
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2020-12-08 20:51:44 -0300

    chore: intl cleanup

--- app/src/components/atoms/GameMapDifficultySelect.messages.ts
+++ app/src/components/atoms/GameMapDifficultySelect.messages.ts
@@ -2,98 +2,122 @@ import { defineMessages } from 'react-intl';
 
 const MapModeSelectMessages = defineMessages({
   'std.easy': {
+    id: 'components.atoms.gameMapDifficultySelect.standardEasy',
     description: 'Standard Easy diffculty name',
     defaultMessage: 'Easy',
   },
   'std.normal': {
+    id: 'components.atoms.gameMapDifficultySelect.standardNormal',
     description: 'Standard Normal diffculty name',
     defaultMessage: 'Normal',
   },
   'std.hard': {
+    id: 'components.atoms.gameMapDifficultySelect.standardHard',
     description: 'Standard Hard difficulty name',
     defaultMessage: 'Hard',
   },
   'std.insane': {
+    id: 'components.atoms.gameMapDifficultySelect.standardInsane',
     description: 'Standard Insane difficulty name',
     defaultMessage: 'Insane',
   },
   'std.expert': {
+    id: 'components.atoms.gameMapDifficultySelect.standardExpert',
     description: 'Standard Expert difficulty name',
     defaultMessage: 'Expert',
   },
   'std.expert+': {
+    id: 'components.atoms.gameMapDifficultySelect.standardExpertPlus',
     description: 'Standard Expert Plus difficulty name',
     defaultMessage: 'Expert+',
   },
   'taiko.easy': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoEasy',
     description: 'Taiko Easy diffculty name',
     defaultMessage: 'Kantan',
   },
   'taiko.normal': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoNormal',
     description: 'Taiko Normal diffculty name',
     defaultMessage: 'Futsuu',
   },
   'taiko.hard': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoHard',
     description: 'Taiko Hard difficulty name',
     defaultMessage: 'Muzukashii',
   },
   'taiko.insane': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoInsane',
     description: 'Taiko Insane difficulty name',
     defaultMessage: 'Oni',
   },
   'taiko.expert': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoExpert',
     description: 'Taiko Expert difficulty name',
     defaultMessage: 'Inner/Ura Oni',
   },
   'taiko.expert+': {
+    id: 'components.atoms.gameMapDifficultySelect.taikoExpertPlus',
     description: 'Taiko Expert Plus difficulty name',
     defaultMessage: 'Hell Oni',
   },
   'ctb.easy': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatEasy',
     description: 'Catch the beat Easy diffculty name',
     defaultMessage: 'Cup',
   },
   'ctb.normal': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatNormal',
     description: 'Catch the beat Normal diffculty name',
     defaultMessage: 'Salad',
   },
   'ctb.hard': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatHard',
     description: 'Catch the beat Hard difficulty name',
     defaultMessage: 'Platter',
   },
   'ctb.insane': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatInsane',
     description: 'Catch the beat Insane difficulty name',
     defaultMessage: 'Rain',
   },
   'ctb.expert': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatExpert',
     description: 'Catch the beat Expert difficulty name',
     defaultMessage: 'Overdose',
   },
   'ctb.expert+': {
+    id: 'components.atoms.gameMapDifficultySelect.catchTheBeatExpertPlus',
     description: 'Catch the beat Expert Plus difficulty name',
     defaultMessage: 'Overdose+',
   },
   'mania.easy': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaEasy',
     description: 'Mania Easy diffculty name',
     defaultMessage: 'Easy',
   },
   'mania.normal': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaNormal',
     description: 'Mania Normal diffculty name',
     defaultMessage: 'Normal',
   },
   'mania.hard': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaHard',
     description: 'Mania Hard difficulty name',
     defaultMessage: 'Hard',
   },
   'mania.insane': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaInsane',
     description: 'Mania Insane difficulty name',
     defaultMessage: 'Insane',
   },
   'mania.expert': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaExpert',
     description: 'Mania Expert difficulty name',
     defaultMessage: 'Expert',
   },
   'mania.expert+': {
+    id: 'components.atoms.gameMapDifficultySelect.maniaExpertPlus',
     description: 'Mania Expert Plus difficulty name',
     defaultMessage: 'Expert+',
   },
--- app/src/components/molecules/GameMapModeSelect.messages.ts
+++ app/src/components/molecules/GameMapModeSelect.messages.ts
@@ -2,18 +2,22 @@ import { defineMessages } from 'react-intl';
 
 const MapModeSelectMessages = defineMessages({
   ctb: {
+    id: 'components.atoms.form-controls.catchOptionLabel',
     description: 'Osu!catch option label',
     defaultMessage: 'Catch',
   },
   taiko: {
+    id: 'components.atoms.form-controls.taikoOptionLabel',
     description: 'Osu!taiko option label',
     defaultMessage: 'Taiko',
   },
   std: {
+    id: 'components.atoms.form-controls.standardOptionLabel',
     description: 'Osu! option label',
     defaultMessage: 'Standard',
   },
   mania: {
+    id: 'components.atoms.form-controls.maniaOptionLabel',
     description: 'Osu!mania option label',
     defaultMessage: 'Mania',
   },
