ba3aa3e4d3a41534c2c886da993c3b1cf7646a22
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-04 21:27:29 -0300

    feat: added colored status badges

--- app/src/components/atoms/JourneyStatusBadge.tsx
+++ app/src/components/atoms/JourneyStatusBadge.tsx
@@ -1,4 +1,4 @@
-import 'twin.macro';
+import tw, { styled } from 'twin.macro';
 import React, { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 
@@ -17,10 +17,37 @@ const JourneyStatusBadge: React.FC<{ status: Journey['status'] }> = (props) => {
     }
   }, [props.status]);
   return (
-    <section tw="flex-none text-sm border rounded-full text-center p-2 bg-white font-bold dark:(text-black)">
+    <section {...props}>
       <FormattedMessage {...statusMessage} />
     </section>
   );
 };
 
-export default JourneyStatusBadge;
+export default styled(JourneyStatusBadge)((props) => [
+  tw`flex-none text-sm border rounded-full text-center p-2 bg-white font-bold dark:(text-black)`,
+  props.status === 'pending' &&
+    tw`
+      light:(bg-orange-100 text-orange-900 border-orange-300)
+      dark:(bg-orange-700 text-orange-100 border-orange-900)
+    `,
+  props.status === 'open' &&
+    tw`
+      light:(bg-lightBlue-100 text-lightBlue-900 border-lightBlue-300)
+      dark:(bg-lightBlue-700 text-lightBlue-100 border-lightBlue-900)
+    `,
+  props.status === 'ready' &&
+    tw`
+      light:(bg-green-100 text-green-900 border-green-300)
+      dark:(bg-green-700 text-green-100 border-green-900)
+    `,
+  props.status === 'alert' &&
+    tw`
+      light:(bg-yellow-100 text-yellow-900 border-yellow-300)
+      dark:(bg-yellow-700 text-yellow-100 border-yellow-900)
+    `,
+  props.status === 'problem' &&
+    tw`
+      light:(bg-red-100 text-red-900 border-red-300)
+      dark:(bg-red-700 text-red-100 border-red-900)
+    `,
+]);
