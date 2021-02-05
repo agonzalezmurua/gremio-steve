b67c9676c17d08dc80ac6e3f74a652b6b7612a5c
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-04 21:29:36 -0300

    fix: removed unintentional scroll on journeypage

--- app/src/pages/journeys/New.tsx
+++ app/src/pages/journeys/New.tsx
@@ -1,6 +1,5 @@
 import 'twin.macro';
 import React, { useCallback } from 'react';
-import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 
 import NewJourneyForm from '@components/templates/journeys/JourneyForm';
 import { JourneyFormObject } from '@components/templates/journeys/JourneyForm.formik';
@@ -11,9 +10,7 @@ const NewJourneyPage: React.FC = () => {
   }, []);
   return (
     <main tw="p-4">
-      <OverlayScrollbarsComponent>
-        <NewJourneyForm onSubmit={handleSubmit} />
-      </OverlayScrollbarsComponent>
+      <NewJourneyForm onSubmit={handleSubmit} />
     </main>
   );
 };
