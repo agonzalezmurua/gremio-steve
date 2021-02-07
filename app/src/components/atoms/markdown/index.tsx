2d7a08eee8f25c66bff0b725c5f0374b186c23f9
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-07 15:18:03 -0300

    feat: cleanup con markdown component

--- app/src/components/atoms/markdown/index.tsx
+++ app/src/components/atoms/markdown/index.tsx
@@ -1,29 +1,18 @@
-import tw, { styled } from 'twin.macro';
+/* eslint-disable react/no-children-prop */
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 
-import ArticleStyle from '_globals/styles/article';
+import Styles from './styles';
 
 type Props = {
-  value: string;
+  text: string;
 } & React.HTMLProps<HTMLDivElement>;
 
-const Container = styled.section`
-  ${tw`light:text-black dark:text-gray-300`}
-  ul {
-    li {
-      list-style-type: disc;
-      list-style-position: outside;
-      ${tw`ml-12`}
-    }
-  }
-`;
-
-const Markdown: React.FC<Props> = ({ value, ...props }) => (
-  <Container {...props} css={[ArticleStyle]}>
-    <ReactMarkdown plugins={[gfm]} children={value} />
-  </Container>
+const Markdown: React.FC<Props> = ({ text, ...props }) => (
+  <Styles.Container {...props}>
+    <ReactMarkdown plugins={[gfm]} children={text} />
+  </Styles.Container>
 );
 
 export default Markdown;
