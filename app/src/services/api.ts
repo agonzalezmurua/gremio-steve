0cf2184612f19b4681cf546c101aacf36e2285b9
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-11 11:31:25 -0300

    wip: integration of api

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,31 +1,47 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { operations, definitions } from '_typings/api.gremio-steve';
 
 axios.defaults.baseURL = CONFIG.app.api.uri;
 
-const api = {
+type JourneyOperations = {
+  search: (
+    parameters: operations['searchJourneys']['parameters']
+  ) => Promise<AxiosResponse<Array<definitions['Journey']>>>;
+};
+
+const api: {
+  journeys: JourneyOperations;
+} = {
   journeys: {
-    search(search: string) {
-      return axios.get<Journey[]>(`/journeys`, {
-        params: {
-          search: search,
-        },
+    search: (params) => {
+      return axios.get<Array<definitions['Journey']>>(`/journeys`, {
+        params: params.query,
       });
     },
-    getById(id: string) {
-      return axios.get<Journey>(`/journeys/${id}`);
-    },
   },
-  user: {
-    profile(id: string) {
-      return axios.get<User>(`/user/${id}`);
-    },
-    queue(id: string) {
-      return axios.get<Journey[]>(`/user/${id}/queue`);
-    },
-    activity(id: string) {
-      return axios.get<UserActivity[]>(`/user/${id}/activity`);
-    },
-  },
+  // journeys: {
+  //   search(search: string) {
+  //     return axios.get<Journey[]>(`/journeys`, {
+  //       params: {
+  //         search: search,
+  //       },
+  //     });
+  //   },
+  //   getById(id: string) {
+  //     return axios.get<Journey>(`/journeys/${id}`);
+  //   },
+  // },
+  // user: {
+  //   profile(id: string) {
+  //     return axios.get<User>(`/user/${id}`);
+  //   },
+  //   queue(id: string) {
+  //     return axios.get<Journey[]>(`/user/${id}/queue`);
+  //   },
+  //   activity(id: string) {
+  //     return axios.get<UserActivity[]>(`/user/${id}/activity`);
+  //   },
+  // },
 };
 
 export default api;
