25ffdda871225c39f44a5598d8d63a4a3b3dd3d0
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-19 15:09:56 -0300

    feat(templates/journeys): simplified conditional rendering

--- src/renderer/components/templates/journeys/index.tsx
+++ src/renderer/components/templates/journeys/index.tsx
@@ -24,19 +24,19 @@ const JourneyTemplate: React.FC<JourneyTemplateProps> = ({ journey }) => {
 
       <section tw="mx-4 -mt-24 p-3 border space-y-2 rounded-lg bg-white border-gray-300 dark:(bg-gray-900 border-gray-500)">
         <section tw="flex flex-col border-b space-y-2 pb-2 border-gray-300 dark:(border-gray-500)">
-          {journey?.title ? (
+          {journey ? (
             <h1>{journey.title}</h1>
           ) : (
             <SkeletonLoader tw="w-72 h-8" />
           )}
-          {journey?.artist ? (
+          {journey ? (
             <span tw="font-semibold">{journey.artist}</span>
           ) : (
             <SkeletonLoader tw="w-40 h-6" />
           )}
         </section>
         <section tw="flex">
-          {journey?.status ? (
+          {journey ? (
             <JourneyStatusBadge status={journey.status} />
           ) : (
             <SkeletonLoader tw="w-16 h-9" />
@@ -46,32 +46,36 @@ const JourneyTemplate: React.FC<JourneyTemplateProps> = ({ journey }) => {
 
       <section tw="p-4 space-y-8">
         <section tw="space-y-4">
-          {(journey?.organizer && (
+          {journey ? (
             <FormattedMessage
               id="components.templates.journeys.organizerHeader"
               defaultMessage="Organizer"
               description="Journey page, organizer header"
               tagName="h2"
             />
-          )) || <SkeletonLoader tw="w-32 h-6" />}
+          ) : (
+            <SkeletonLoader tw="w-32 h-6" />
+          )}
           {/* Organizer */}
           <section tw="flex flex-grow space-x-2">
-            {(journey?.organizer?.name && (
+            {journey ? (
               <UserCard
-                _id={journey.organizer.id}
+                id={journey.organizer.id}
                 availability={journey.organizer.availability}
                 avatar_url={journey.organizer.avatar_url}
                 community_role={journey.organizer.community_role}
                 name={journey.organizer.name}
                 preferences={journey.organizer.preferences}
               />
-            )) || <SkeletonLoader tw="w-full h-28" />}
+            ) : (
+              <SkeletonLoader tw="w-full h-28" />
+            )}
           </section>
         </section>
 
         {/* Maps */}
         <section tw="space-y-4">
-          {(journey?.beatmaps && (
+          {journey ? (
             <>
               <FormattedMessage
                 id="components.templates.journeys.difficultiesHeader"
@@ -81,7 +85,7 @@ const JourneyTemplate: React.FC<JourneyTemplateProps> = ({ journey }) => {
               />
               <GameMapTable maps={journey.beatmaps} />
             </>
-          )) || (
+          ) : (
             <>
               <SkeletonLoader tw="w-32 h-6" />
               <SkeletonLoader tw="w-full h-64" />
