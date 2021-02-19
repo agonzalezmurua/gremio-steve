dcc67bc5f7e2cb13fc9073f5d44ff83594eddb5c
Agustín González <8430727+agonzalezmurua@users.noreply.github.com>
2021-02-19 15:10:16 -0300

    feat(constants/journeys): added journey constants

---
+++ src/renderer/constants/journey.ts
@@ -0,0 +1,14 @@
+import { Definitions } from '@/services/api';
+
+export const STATUS: {
+  [key in Uppercase<Definitions['Journey']['status']>]: Lowercase<key>;
+} = {
+  ALERT: 'alert',
+  CLOSED: 'closed',
+  OPEN: 'open',
+  PENDING: 'pending',
+  PROBLEM: 'problem',
+  READY: 'ready',
+};
+
+Object.freeze(STATUS);
